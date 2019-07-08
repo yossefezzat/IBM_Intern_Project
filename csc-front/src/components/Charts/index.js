2ca49b0a948189369647b5aace54c1e8b18eeeea
Mahmoud Atwa <m.atwa@articlebox.net>
2019-07-08 12:48:54 +0200

    Bootstrapped App

---
+++ csc-front/src/components/Charts/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+export default class Charts extends React.Component {
+
+    render(){
+        return <NavLink activeClassName="kk" to="/">Hi</NavLink>
+    }
+}
---
+++ csc-front/src/components/NavBar/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class NavBar extends React.Component {
+
+    render(){
+        return <h1>NavBar</h1>
+    }
+}
