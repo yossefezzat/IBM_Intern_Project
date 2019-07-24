a027aea3249805fafa657275bd2017d211b3a7fe
yossefezzat <45318576+yossefezzat@users.noreply.github.com>
2019-07-24 13:39:17 +0200

    backend

---
+++ twitter.js
@@ -0,0 +1,78 @@
+require('dotenv').config();
+var TwitterStream = require('twitter-stream-api'),
+    fs = require('fs');
+var keys = {
+        // consumer_key : "FPi2NDvj6gyOX4wQht4KgxRhd",
+        // consumer_secret : "USFAQ5h8kTnax5AdS6oA7JCvfJvTKzrVeppa51STj3K8eO2tnm",
+        // token : "1098634432154611712-lnCImR0e6Z6E2XlRF6vCD0aHB0KhXM",
+        // token_secret : "oCyIwr8rVDMFaOusSxSTSlywMqByTQauN6HjwX8m1GYXO"
+
+        consumer_key : process.env.CONSUMER_KEY + "",
+        consumer_secret : process.env.CONSUMER_SECRET + "",
+        token : process.env.TOKEN + "",
+        token_secret : process.env.TOKEN_SECRET + ""
+    };
+
+function init(insert, env){
+    
+    console.log(keys);
+
+    var Twitter = new TwitterStream(keys, false);
+    Twitter.stream('statuses/filter', {
+        track: '#ibm'
+    });
+
+Twitter.on('connection error network', function (error) {
+    console.log('connection error network', error);
+});
+Twitter.on('connection aborted', function () {
+    console.log('connection aborted');
+});
+Twitter.on('connection success', function (uri) {
+    console.log('connection success', uri);
+});
+Twitter.on('connection error stall', function () {
+    console.log('connection error stall');
+});
+Twitter.on('connection error http', function (httpStatusCode) {
+    console.log('connection error http', httpStatusCode);
+});
+Twitter.on('connection rate limit', function (httpStatusCode) {
+    console.log('connection rate limit', httpStatusCode);
+});
+Twitter.on('connection error unknown', function (error) {
+    console.log('connection error unknown', error);
+    Twitter.close();
+});
+     Twitter.on('data',  async (obj) => {
+        const tweet = JSON.parse(obj.toString());
+        const timestamp = tweet.created_at;
+        const tweetID = tweet.id;
+        let tweetText = tweet.text;
+        const extended = tweet.extended_tweet;
+        if(tweet.truncated){
+            tweetText = extended.full_text;
+        }
+        let entities = (extended || {}).entities || tweet.entities;
+        let username = tweet.user.screen_name;
+        let displayName = tweet.user.name;
+        let userID = tweet.user.id;
+        let userImageURL = tweet.user.profile_image_url_https;
+        let tweet_toStore = {
+            timestamp, 
+            tweetID, 
+            tweetText, 
+            entities, 
+            username, 
+            displayName, 
+            userID, 
+            userImageURL
+        }
+    
+        insert(tweet_toStore);
+    })
+    
+}
+    
+
+module.exports = init ;
